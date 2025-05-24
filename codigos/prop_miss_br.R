afe225f5e406dc5fcae072c080ec1989fc3a5a8b
mirnatetzner <mirnatetzner@gmail.com>
2025-05-24 00:12:26 -0300

    gera de novo, corta dados da idade do pai para 50 anos para facilitar a comparacao com  tft feminina

--- codigos/prop_miss_br.R
+++ codigos/prop_miss_br.R
@@ -9,6 +9,7 @@ options(scipen = 999,
         OutDec = ",")
 
 UFs = c("AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO")
+
 for (UF in UFs) {
   nome <- UF
   assign(nome, fetch_datasus(year_start = 2012, year_end = 2022, uf = UF, 
@@ -23,34 +24,51 @@ library(lubridate)
 dados_brasil <- bind_rows(lapply(UFs, function(UF) {
   dados <- get(UF)
   dados$UF <- UF
-  dados$Ano <- year(as.Date(dados$DTNASC, format = "%d%m%Y"))  # <- aqui está a conversão correta
+  dados$Ano <- year(as.Date(dados$DTNASC, format = "%d%m%Y"))  
   return(dados)
 }))
+
+
+
+
 # Certifique-se de que o campo de data está em formato Date
 dados_brasil$Ano <- year(as.Date(dados_brasil$DTNASC))
 
-# Cálculo da proporção de missing por ano e UF
-missing_por_ano_uf <- dados_brasil %>%
+library(dplyr)
+
+proporcao_por_ano <- dados_brasil %>%
   group_by(Ano) %>%
-  summarise(
-    total = n(),
-    missing_idadepai = sum(is.na(IDADEPAI)),
-    prop_missing = missing_idadepai / total,
-    .groups = "drop"
-  )
+  summarise(proporcao_faltantes = mean(is.na(IDADEPAI)) *100)
+
+print(proporcao_por_ano)
 
 
 library(ggplot2)
 
-ggplot(missing_por_ano_uf, aes(x = Ano, y = prop_missing, color = UF)) +
-  geom_line(size = 1) +
-  geom_point(size = 2) +
-  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+
+
+
+# Gráfico de linha do tempo
+grap_plot_missing <- ggplot(proporcao_por_ano, aes(x = as.factor(Ano), y = proporcao_faltantes, group = 1)) +
+  geom_line(size = 1) +  # Linhas conectando os pontos
+  geom_point(size = 2) + # Pontos sobre as linhas
+  scale_y_continuous(limits = c(0, 100), labels = scales::percent_format(scale = 1)) +  # Define os limites do eixo y
   labs(
-    title = "Proporção de valores ausentes em IDADEPAI por UF (2012–2022)",
+    title = "",
     x = "Ano",
-    y = "% de missing",
-    color = "UF"
+    y = "Percentual de Valores Ausentes \n para a idade do pai(%)",
+    color = "Estado:",
+    linetype = "Estado:"
   ) +
-  theme_minimal(base_size = 14) +
-  theme(legend.position = "bottom")
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
+    legend.position = "bottom",
+    legend.text = element_text(size = 14),
+    panel.grid=element_line(color="grey75"),
+    title=element_text(color="black",size=14),
+    axis.text = element_text(color="black",size=14),
+    axis.title = element_text(color="black",size=14)
+  )
+grap_plot_missing
+ggsave("faltantes_brasil.png",plot = grap_plot_missing, width = 10, height = 6, path = "D:/Mirna/ENCE/DISSERTACAO/Dissertacao_text/imagens", dpi = 300)
